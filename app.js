8a9ce347625e19134918bdcb705a6c19898a8d68
Cameron <camsjobhunting@gmail.com>
2022-08-11 23:24:42 +0100

    add notes about what to do

---
+++ app.js
@@ -0,0 +1,8 @@
+//goal is to make a score tracking game uisng html, css & js.
+
+// >there should a score keeper,
+// >an option to selece to what you play too,
+// >two buttons to add points, one for each plater,
+// >a button to rest
+
+// >when a player wins the winning number should turn green and the score adding buttons should be disabled
