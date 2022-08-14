2375f37a10ab6dafcae2dcfe8877a201f01d992e
Cameron <camsjobhunting@gmail.com>
2022-08-14 20:36:06 +0100

    add function to score buttons

--- app.js
+++ app.js
@@ -6,3 +6,21 @@
 // >a button to rest
 
 // >when a player wins the winning number should turn green and the score adding buttons should be disabled
+
+//to start lets call out all the elements we're going to need
+
+//player scores
+const playerOneScore = document.querySelector('#player1');
+const playerTwoScore = document.querySelector('#player2');
+
+//add one to scores
+const pointPOne = document.querySelector('#pointPOne')
+const pointPTwo = document.querySelector('#pointPTwo')
+
+pointPOne.addEventListener('click', () => {
+    playerOneScore.innerHTML = Number(playerOneScore.innerHTML) + 1;
+})
+
+pointPTwo.addEventListener('click', () => {
+    playerTwoScore.innerHTML = Number(playerTwoScore.innerHTML) + 1;
+})
